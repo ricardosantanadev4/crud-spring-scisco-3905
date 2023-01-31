a731e2bf44b2886db81faf6a70c57b08fe2be2d1
ricardosantanadev4 <ricardosantanadev4@gmail.com>
2023-01-31 20:35:31 -0300

    update

--- src/main/java/br/com/rsds/crudspringcisco3905/repository/RamaisRepository.java
+++ src/main/java/br/com/rsds/crudspringcisco3905/repository/RamaisRepository.java
@@ -1,9 +1,11 @@
 package br.com.rsds.crudspringcisco3905.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.rsds.crudspringcisco3905.model.RamaisList;
 
-public interface RamaisRepository extends JpaRepository<RamaisList, Long>{
-	
+@Repository
+public interface RamaisRepository extends JpaRepository<RamaisList, Long> {
+
 }
